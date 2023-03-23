e876d82bd90cc5c1b8f3f0430b9bc4efed5151a4
hazadus <hazadus7@gmail.com>
2023-03-23 20:44:41 +0300

    feat(backend/products): added DRF API endpoint for Product detail view.

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import LatestProductsList
+from .views import LatestProductsList, ProductDetailView
 
 urlpatterns = [
     path("latest-products/", LatestProductsList.as_view()),
+    path(
+        "products/<slug:category_slug>/<slug:product_slug>/",
+        ProductDetailView.as_view(),
+    ),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,7 @@
+"""
+DRF API views for Products.
+"""
+from django.http import Http404
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,7 +12,7 @@ from .serializers import ProductSerializer
 
 class LatestProductsList(APIView):
     """
-    View for latest products. Return 4 newest products in the shop.
+    API endpoint for latest products. Return 4 newest products in the shop.
     """
 
     @staticmethod
@@ -18,4 +22,35 @@ class LatestProductsList(APIView):
         """
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(instance=products, many=True)
+        return Response(serializer.data)
+
+
+class ProductDetailView(APIView):
+    """
+    API endpoint for product details.
+    """
+
+    @staticmethod
+    def get_object(category_slug: str, product_slug: str):
+        """
+        Return product instance.
+        """
+        product = Product.objects.filter(
+            category__slug=category_slug,
+            slug=product_slug,
+        )
+
+        if product:
+            return product.first()
+
+        raise Http404
+
+    def get(self, request: Request, category_slug: str, product_slug: str) -> Response:
+        """
+        Return product data by category and product slugs.
+        """
+        product = self.get_object(
+            category_slug=category_slug, product_slug=product_slug
+        )
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
